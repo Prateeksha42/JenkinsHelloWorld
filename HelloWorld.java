de8320e5b495ac2620f71606b1bb7f27575ce439
Prateeksha42 <53993726+Prateeksha42@users.noreply.github.com>
2021-05-07 12:29:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Updated the Hello world code!");
+    System.out.println("Updated Hello world!");
   }
 }
